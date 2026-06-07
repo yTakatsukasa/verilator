928316497199a17661285d910623a243a726b6e6
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2026-06-07 14:07:25 +0900

    Add subgraph system-function constant regressions

--- test_regress/t/t_subgraph_boundary.v
+++ test_regress/t/t_subgraph_boundary.v
@@ -26,15 +26,15 @@ module t (
 
   int cyc;
   logic [7:0] in;
-  logic [167:0] out0;
-  logic [167:0] out1;
-  logic [167:0] ref0;
-  logic [167:0] ref1;
+  logic [183:0] out0;
+  logic [183:0] out1;
+  logic [183:0] ref0;
+  logic [183:0] ref1;
 
-  sub i_sub0 (.clk(clk), .in(in), .out_child_comb(out0[95:88]), .out_child_direct(out0[87:80]), .out_func_default(out0[79:72]), .out_func_local_init(out0[71:64]), .out_func_return(out0[63:56]), .out_gen_always_comb(out0[55:48]), .out_gen_assign(out0[47:40]), .out_gen_child(out0[39:32]), .out_gen_concat(out0[111:104]), .out_gen_localparam_const(out0[159:152]), .out_gen_localparam_partsel(out0[167:160]), .out_gen_packed_func(out0[103:96]), .out_gen_param_const(out0[143:136]), .out_gen_param_partsel(out0[151:144]), .out_gen_partsel(out0[119:112]), .out_gen_struct(out0[127:120]), .out_gen_unpacked(out0[135:128]), .out_genvar_value(out0[31:24]), .out_reg_child_comb(out0[23:16]), .out_reg_child_func(out0[15:8]), .out_reg_comb(out0[7:0]));
-  sub i_sub1 (.clk(clk), .in(in + 8'd1), .out_child_comb(out1[95:88]), .out_child_direct(out1[87:80]), .out_func_default(out1[79:72]), .out_func_local_init(out1[71:64]), .out_func_return(out1[63:56]), .out_gen_always_comb(out1[55:48]), .out_gen_assign(out1[47:40]), .out_gen_child(out1[39:32]), .out_gen_concat(out1[111:104]), .out_gen_localparam_const(out1[159:152]), .out_gen_localparam_partsel(out1[167:160]), .out_gen_packed_func(out1[103:96]), .out_gen_param_const(out1[143:136]), .out_gen_param_partsel(out1[151:144]), .out_gen_partsel(out1[119:112]), .out_gen_struct(out1[127:120]), .out_gen_unpacked(out1[135:128]), .out_genvar_value(out1[31:24]), .out_reg_child_comb(out1[23:16]), .out_reg_child_func(out1[15:8]), .out_reg_comb(out1[7:0]));
-  sub_ref i_ref0 (.clk(clk), .in(in), .out_child_comb(ref0[95:88]), .out_child_direct(ref0[87:80]), .out_func_default(ref0[79:72]), .out_func_local_init(ref0[71:64]), .out_func_return(ref0[63:56]), .out_gen_always_comb(ref0[55:48]), .out_gen_assign(ref0[47:40]), .out_gen_child(ref0[39:32]), .out_gen_concat(ref0[111:104]), .out_gen_localparam_const(ref0[159:152]), .out_gen_localparam_partsel(ref0[167:160]), .out_gen_packed_func(ref0[103:96]), .out_gen_param_const(ref0[143:136]), .out_gen_param_partsel(ref0[151:144]), .out_gen_partsel(ref0[119:112]), .out_gen_struct(ref0[127:120]), .out_gen_unpacked(ref0[135:128]), .out_genvar_value(ref0[31:24]), .out_reg_child_comb(ref0[23:16]), .out_reg_child_func(ref0[15:8]), .out_reg_comb(ref0[7:0]));
-  sub_ref i_ref1 (.clk(clk), .in(in + 8'd1), .out_child_comb(ref1[95:88]), .out_child_direct(ref1[87:80]), .out_func_default(ref1[79:72]), .out_func_local_init(ref1[71:64]), .out_func_return(ref1[63:56]), .out_gen_always_comb(ref1[55:48]), .out_gen_assign(ref1[47:40]), .out_gen_child(ref1[39:32]), .out_gen_concat(ref1[111:104]), .out_gen_localparam_const(ref1[159:152]), .out_gen_localparam_partsel(ref1[167:160]), .out_gen_packed_func(ref1[103:96]), .out_gen_param_const(ref1[143:136]), .out_gen_param_partsel(ref1[151:144]), .out_gen_partsel(ref1[119:112]), .out_gen_struct(ref1[127:120]), .out_gen_unpacked(ref1[135:128]), .out_genvar_value(ref1[31:24]), .out_reg_child_comb(ref1[23:16]), .out_reg_child_func(ref1[15:8]), .out_reg_comb(ref1[7:0]));
+  sub i_sub0 (.clk(clk), .in(in), .out_child_comb(out0[95:88]), .out_child_direct(out0[87:80]), .out_func_default(out0[79:72]), .out_func_local_init(out0[71:64]), .out_func_return(out0[63:56]), .out_gen_always_comb(out0[55:48]), .out_gen_assign(out0[47:40]), .out_gen_child(out0[39:32]), .out_gen_concat(out0[111:104]), .out_gen_localparam_const(out0[159:152]), .out_gen_localparam_partsel(out0[167:160]), .out_gen_packed_func(out0[103:96]), .out_gen_param_const(out0[143:136]), .out_gen_param_partsel(out0[151:144]), .out_gen_partsel(out0[119:112]), .out_gen_struct(out0[127:120]), .out_gen_sysconst(out0[175:168]), .out_gen_sysrepl(out0[183:176]), .out_gen_unpacked(out0[135:128]), .out_genvar_value(out0[31:24]), .out_reg_child_comb(out0[23:16]), .out_reg_child_func(out0[15:8]), .out_reg_comb(out0[7:0]));
+  sub i_sub1 (.clk(clk), .in(in + 8'd1), .out_child_comb(out1[95:88]), .out_child_direct(out1[87:80]), .out_func_default(out1[79:72]), .out_func_local_init(out1[71:64]), .out_func_return(out1[63:56]), .out_gen_always_comb(out1[55:48]), .out_gen_assign(out1[47:40]), .out_gen_child(out1[39:32]), .out_gen_concat(out1[111:104]), .out_gen_localparam_const(out1[159:152]), .out_gen_localparam_partsel(out1[167:160]), .out_gen_packed_func(out1[103:96]), .out_gen_param_const(out1[143:136]), .out_gen_param_partsel(out1[151:144]), .out_gen_partsel(out1[119:112]), .out_gen_struct(out1[127:120]), .out_gen_sysconst(out1[175:168]), .out_gen_sysrepl(out1[183:176]), .out_gen_unpacked(out1[135:128]), .out_genvar_value(out1[31:24]), .out_reg_child_comb(out1[23:16]), .out_reg_child_func(out1[15:8]), .out_reg_comb(out1[7:0]));
+  sub_ref i_ref0 (.clk(clk), .in(in), .out_child_comb(ref0[95:88]), .out_child_direct(ref0[87:80]), .out_func_default(ref0[79:72]), .out_func_local_init(ref0[71:64]), .out_func_return(ref0[63:56]), .out_gen_always_comb(ref0[55:48]), .out_gen_assign(ref0[47:40]), .out_gen_child(ref0[39:32]), .out_gen_concat(ref0[111:104]), .out_gen_localparam_const(ref0[159:152]), .out_gen_localparam_partsel(ref0[167:160]), .out_gen_packed_func(ref0[103:96]), .out_gen_param_const(ref0[143:136]), .out_gen_param_partsel(ref0[151:144]), .out_gen_partsel(ref0[119:112]), .out_gen_struct(ref0[127:120]), .out_gen_sysconst(ref0[175:168]), .out_gen_sysrepl(ref0[183:176]), .out_gen_unpacked(ref0[135:128]), .out_genvar_value(ref0[31:24]), .out_reg_child_comb(ref0[23:16]), .out_reg_child_func(ref0[15:8]), .out_reg_comb(ref0[7:0]));
+  sub_ref i_ref1 (.clk(clk), .in(in + 8'd1), .out_child_comb(ref1[95:88]), .out_child_direct(ref1[87:80]), .out_func_default(ref1[79:72]), .out_func_local_init(ref1[71:64]), .out_func_return(ref1[63:56]), .out_gen_always_comb(ref1[55:48]), .out_gen_assign(ref1[47:40]), .out_gen_child(ref1[39:32]), .out_gen_concat(ref1[111:104]), .out_gen_localparam_const(ref1[159:152]), .out_gen_localparam_partsel(ref1[167:160]), .out_gen_packed_func(ref1[103:96]), .out_gen_param_const(ref1[143:136]), .out_gen_param_partsel(ref1[151:144]), .out_gen_partsel(ref1[119:112]), .out_gen_struct(ref1[127:120]), .out_gen_sysconst(ref1[175:168]), .out_gen_sysrepl(ref1[183:176]), .out_gen_unpacked(ref1[135:128]), .out_genvar_value(ref1[31:24]), .out_reg_child_comb(ref1[23:16]), .out_reg_child_func(ref1[15:8]), .out_reg_comb(ref1[7:0]));
 
   always_ff @(posedge clk) begin
     cyc <= cyc + 1;
@@ -79,6 +79,8 @@ module sub #(
   output logic [7:0] out_gen_param_partsel,
   output logic [7:0] out_gen_partsel,
   output logic [7:0] out_gen_struct,
+  output logic [7:0] out_gen_sysconst,
+  output logic [7:0] out_gen_sysrepl,
   output logic [7:0] out_gen_unpacked,
   output logic [7:0] out_genvar_value,
   output logic [7:0] out_reg_child_comb,
@@ -186,6 +188,8 @@ module sub #(
                                  ^ {PARAM_BIAS[PARAM_TOP -: PARAM_MID],
                                     PARAM_BIAS[0 +: (8 - PARAM_MID)]};
   assign out_gen_struct = gen_struct;
+  assign out_gen_sysconst = {q[$left(q) -: 4], q[$right(q) +: 3], $bits(q) == 8} ^ 8'h5c;
+  assign out_gen_sysrepl = {$bits(q){q[0]}} ^ 8'h3a;
   assign out_gen_unpacked = {gen_unpacked[3], gen_unpacked[2], gen_unpacked[1], gen_unpacked[0]};
   assign out_reg_child_comb = reg_child_out ^ 8'h42;
   assign out_reg_child_func = reg_child_func_out ^ 8'h24;
@@ -216,6 +220,8 @@ module sub_ref #(
   output logic [7:0] out_gen_param_partsel,
   output logic [7:0] out_gen_partsel,
   output logic [7:0] out_gen_struct,
+  output logic [7:0] out_gen_sysconst,
+  output logic [7:0] out_gen_sysrepl,
   output logic [7:0] out_gen_unpacked,
   output logic [7:0] out_genvar_value,
   output logic [7:0] out_reg_child_comb,
@@ -322,6 +328,8 @@ module sub_ref #(
                                  ^ {PARAM_BIAS[PARAM_TOP -: PARAM_MID],
                                     PARAM_BIAS[0 +: (8 - PARAM_MID)]};
   assign out_gen_struct = gen_struct;
+  assign out_gen_sysconst = {q[$left(q) -: 4], q[$right(q) +: 3], $bits(q) == 8} ^ 8'h5c;
+  assign out_gen_sysrepl = {$bits(q){q[0]}} ^ 8'h3a;
   assign out_gen_unpacked = {gen_unpacked[3], gen_unpacked[2], gen_unpacked[1], gen_unpacked[0]};
   assign out_reg_child_comb = reg_child_out ^ 8'h42;
   assign out_reg_child_func = reg_child_func_out ^ 8'h24;
