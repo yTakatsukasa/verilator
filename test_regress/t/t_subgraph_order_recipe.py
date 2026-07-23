ac9008a403a8629fac49b0d4531bebdd86e1b927
github action <action@example.com>
2026-07-23 00:07:40 +0000

    Apply 'make format' [ci skip]

--- test_regress/t/t_subgraph_order_recipe.py
+++ test_regress/t/t_subgraph_order_recipe.py
@@ -34,8 +34,7 @@ test.file_grep(test.stats,
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, order cache shared skip constants\s+(\d+)",
                0)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, order cache shared skip other\s+(\d+)", 0)
-test.file_grep(test.stats, r"Scheduling, Subgraph nba, order cache shared skip var map\s+(\d+)",
-               2)
+test.file_grep(test.stats, r"Scheduling, Subgraph nba, order cache shared skip var map\s+(\d+)", 2)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, ordered function clones\s+(\d+)", 4)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper constant args\s+(\d+)", 0)
 
--- test_regress/t/t_subgraph_shared_args.py
+++ test_regress/t/t_subgraph_shared_args.py
@@ -14,15 +14,14 @@ test.scenarios("vlt")
 test.compile(verilator_flags2=["--output-split-cfuncs", "10", "--subgraph-schedule", "--stats"])
 test.execute()
 
-test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper formal args after\s+(\d+)",
-               0)
+test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper formal args after\s+(\d+)", 0)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper formal args before\s+(\d+)",
                392)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper constant args\s+(\d+)", 0)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper call args\s+(\d+)", 0)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper call args max\s+(\d+)", 0)
-test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper implicit context vars\s+(\d+)",
-               32)
+test.file_grep(test.stats,
+               r"Scheduling, Subgraph nba, shared helper implicit context vars\s+(\d+)", 32)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper formal args max\s+(\d+)", 0)
 test.file_grep(test.stats, r"Scheduling, Subgraph nba, shared helper instance local args\s+(\d+)",
                0)
